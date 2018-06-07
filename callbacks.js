bc961d086a93557c5a0a78fddbf2878bf4481fdb
danitiemi <danitiemi.br@gmail.com>
2018-06-07 19:07:11 +0000

    exercises completed

---
+++ callbacks.js
@@ -0,0 +1,35 @@
+// The second argument/parameter is expected to be a function
+
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      index = i;
+      found(index);   // execute callback
+    }
+  }
+}
+
+function actionWhenFound(index) {
+  console.log("Found Waldo at index " + index + "!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
+function findWaldo(array, found) {
+  array.forEach(function(element, index) {
+    if (element === "Waldo") {
+      found(index);   // execute callback
+    }
+  });
+}
+
+function actionWhenFound(index) {
+  console.log("Found Waldo at index " + index + "!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
